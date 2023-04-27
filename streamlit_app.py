41de3c9c9d2e55242e5d4e8cd61888ca26ba2c92
timberwolf12o9 <35613759+timberwolf12o9@users.noreply.github.com>
2023-04-26 21:37:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,49 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+"""Main module for the streamlit app"""
 import streamlit as st
 
+import awesome_streamlit as ast
+import src.pages.about
+import src.pages.gallery.index
+import src.pages.home
+import src.pages.resources
+import src.pages.vision
+
+ast.core.services.other.set_logging_format()
+
+PAGES = {
+    "Home": src.pages.home,
+    "Resources": src.pages.resources,
+    "Gallery": src.pages.gallery.index,
+    "Vision": src.pages.vision,
+    "About": src.pages.about,
+}
+
+
+def main():
+    """Main function of the App"""
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+    page = PAGES[selection]
+
+    with st.spinner(f"Loading {selection} ..."):
+        ast.shared.components.write_page(page)
+    st.sidebar.title("Contribute")
+    st.sidebar.info(
+        "This an open source project and you are very welcome to **contribute** your awesome "
+        "comments, questions, resources and apps as "
+        "[issues](https://github.com/MarcSkovMadsen/awesome-streamlit/issues) of or "
+        "[pull requests](https://github.com/MarcSkovMadsen/awesome-streamlit/pulls) "
+        "to the [source code](https://github.com/MarcSkovMadsen/awesome-streamlit). "
+    )
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This app is maintained by Marc Skov Madsen. You can learn more about me at
+        [datamodelsanalytics.com](https://datamodelsanalytics.com).
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    )
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
